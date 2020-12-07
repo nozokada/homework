1ec267336ccd8832f7c98f25e5ca2686fbcc0f64
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 19:24:43 -0700

    Added test_create_books_with_bad_format

--- tests/api/test_create_books.py
+++ tests/api/test_create_books.py
@@ -10,7 +10,14 @@ class TestCreateBooks:
         user = create_test_user(teardown=True)
         create_test_books(user_id=user.user_id, teardown=True)
 
-    def test_create_books_not_authorized(self, create_test_user, bookstore):
+    def test_create_books_with_bad_format(self, bookstore, create_test_user):
+        user = create_test_user(teardown=True)
+        collection_of_isbns = [{'isbn': isbn} for isbn in EXISTING_ISBNS]
+        resp = bookstore.add_books(userId=user.user_id, what=collection_of_isbns)
+        assert resp.status_code == HTTPStatus.BAD_REQUEST
+        assert CodeResponse(code='1200', message='UserID and Collection of ISBNs required.')
+
+    def test_create_books_not_authorized(self, bookstore, create_test_user):
         user = create_test_user(teardown=True)
         token = bookstore.session.headers.pop('Authorization')
         collection_of_isbns = [{'isbn': isbn} for isbn in EXISTING_ISBNS]
