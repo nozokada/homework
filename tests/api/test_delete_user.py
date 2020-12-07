d4d50dd60109b0d73c35e0447c2a0e7257fb99e3
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 17:48:37 -0700

    Added checks for response body

--- tests/api/test_delete_user.py
+++ tests/api/test_delete_user.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from homework.models.bookstore import CodeResponse
+
 
 class TestDeleteUser:
 
@@ -7,15 +9,18 @@ class TestDeleteUser:
         user = create_test_user()
         resp = account.delete_user(userId=user.user_id)
         assert resp.status_code == HTTPStatus.NO_CONTENT
+        assert resp.text == ''
 
     def test_delete_nonexistent_user(self, account, create_test_user):
         create_test_user(teardown=True)
         resp = account.delete_user(userId='invalid_user_id')
         assert resp.status_code == HTTPStatus.OK
+        assert CodeResponse(code='1207', message='User Id not correct!')
 
     def test_delete_user_not_authorized(self, account, create_test_user):
         user = create_test_user(teardown=True)
         token = account.session.headers.pop('Authorization')
         resp = account.delete_user(userId=user.user_id)
         assert resp.status_code == HTTPStatus.UNAUTHORIZED
+        assert CodeResponse(code='1200', message='User not authorized!')
         account.session.headers['Authorization'] = token
