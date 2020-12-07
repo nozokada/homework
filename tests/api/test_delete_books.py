d3997e94c1562abf1d7fc1e78fd9059478bab5bc
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 18:49:14 -0700

    Added checks for response body

--- tests/api/test_delete_books.py
+++ tests/api/test_delete_books.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from homework.models.bookstore import CodeResponse
+
 
 class TestDeleteBooks:
 
@@ -8,6 +10,7 @@ class TestDeleteBooks:
         create_test_books(user_id=user.user_id, teardown=True)
         resp = bookstore.delete_books(userId=user.user_id)
         assert resp.status_code == HTTPStatus.NO_CONTENT
+        assert resp.text == ''
 
     def test_delete_books_not_authorized(self, bookstore, create_test_user, create_test_books):
         user = create_test_user(teardown=True)
@@ -15,4 +18,5 @@ class TestDeleteBooks:
         token = bookstore.session.headers.pop('Authorization')
         resp = bookstore.delete_books(userId=user.user_id)
         assert resp.status_code == HTTPStatus.UNAUTHORIZED
+        assert CodeResponse(**resp.json()) == CodeResponse(code='1200', message='User not authorized!')
         bookstore.session.headers['Authorization'] = token
