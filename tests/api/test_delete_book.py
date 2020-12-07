16b80df1d43a17111cbdde03fd55608e239f3056
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 19:21:44 -0700

    Added test_delete_book_with_bad_format

--- tests/api/test_delete_book.py
+++ tests/api/test_delete_book.py
@@ -19,6 +19,12 @@ class TestDeleteBook:
         assert resp.status_code == HTTPStatus.NOT_FOUND
         assert CodeResponse(**resp.json()) == CodeResponse(code='1207', message='Book not found!')
 
+    def test_delete_book_with_bad_format(self, bookstore, create_test_user):
+        user = create_test_user(teardown=True)
+        resp = bookstore.delete_book(isbn=INVALID_ISBNS[0], what=user.user_id)
+        assert resp.status_code == HTTPStatus.BAD_REQUEST
+        assert CodeResponse(**resp.json()) == CodeResponse(code='1200', message='UserID and ISBN required.')
+
     def test_delete_book_not_authorized(self, bookstore, create_test_user, create_test_books):
         user = create_test_user(teardown=True)
         isbn_resp = create_test_books(user_id=user.user_id, teardown=True)
