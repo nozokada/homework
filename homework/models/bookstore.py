3a69ca4b61ebdbb882f718dbed0cd993b5c0e073
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 17:19:21 -0700

    Added checks for response body

--- homework/models/bookstore.py
+++ homework/models/bookstore.py
@@ -4,6 +4,18 @@ from pydantic import Field
 from pydantic.main import BaseModel
 
 
+class CodeResponse(BaseModel):
+    code: str
+    message: str
+
+
+class TokenResponse(BaseModel):
+    token: str = None
+    expires: str = None
+    status: str
+    result: str
+
+
 class Book(BaseModel):
     isbn: str
     title: str
@@ -20,10 +32,3 @@ class User(BaseModel):
     user_id: str = Field(..., alias='userId')
     username: str
     books: List[Book]
-
-
-class TokenPayload(BaseModel):
-    token: str = None
-    expires: str = None
-    status: str
-    result: str
--- tests/api/test_login.py
+++ tests/api/test_login.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from homework.models.bookstore import CodeResponse
+
 
 class TestLogin:
 
@@ -9,6 +11,7 @@ class TestLogin:
         create_test_user(username=username, password=password, teardown=True)
         resp = account.login(userName=username, password=password)
         assert resp.status_code == HTTPStatus.OK
+        assert resp.json() is True
 
     def test_login_bad_format(self, account, create_test_user, username_generator, password_generator):
         username = username_generator()
@@ -16,9 +19,11 @@ class TestLogin:
         create_test_user(username=username, password=password, teardown=True)
         resp = account.login(userName=username, what=password)
         assert resp.status_code == HTTPStatus.BAD_REQUEST
+        assert CodeResponse(**resp.json()) == CodeResponse(code='1200', message='UserName and Password required.')
 
     def test_login_bad_password(self, account, create_test_user, username_generator, password_generator):
         username = username_generator()
         create_test_user(username=username, password=password_generator(), teardown=True)
         resp = account.login(userName=username, password='badpassword')
         assert resp.status_code == HTTPStatus.NOT_FOUND
+        assert CodeResponse(**resp.json()) == CodeResponse(code='1207', message='User not found!')
