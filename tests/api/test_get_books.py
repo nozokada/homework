a3549ccd7c57744aae6896dcb27b7ba2082467ad
Nozomi Okada <nokada@proofpoint.com>
2020-12-06 18:32:14 -0700

    Changed variable names

--- tests/api/test_get_books.py
+++ tests/api/test_get_books.py
@@ -9,6 +9,6 @@ class TestGetBooks:
     def test_get_books(self, bookstore, json_loader):
         resp = bookstore.get_books()
         assert resp.status_code == HTTPStatus.OK
-        actual_books = [Book(**book_info) for book_info in resp.json()['books']]
-        expected_books = [Book(**book_info) for book_info in json_loader(ALL_BOOKS_FILE_PATH)['books']]
+        actual_books = [Book(**book) for book in resp.json()['books']]
+        expected_books = [Book(**book) for book in json_loader(ALL_BOOKS_FILE_PATH)['books']]
         assert actual_books == expected_books
